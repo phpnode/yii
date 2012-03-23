d42146b770b8f2273f2d3cdc2f2faae2983ad952
Charles Pick <charles.pick@gmail.com>
2012-03-23 10:32:45 +0000

    Performance tests

---
+++ tests/framework/CDbReaderPerformanceTest.php
@@ -0,0 +1,54 @@
+<?php
+Yii::import("system.web.*");
+require_once __DIR__."/db/data/models.php";
+class CDbReaderPerformanceTest extends CTestCase {
+	/**
+	 * @var CDbConnection
+	 */
+	private $_connection;
+
+	protected function setUp()
+	{
+		if(!extension_loaded('pdo') || !extension_loaded('pdo_sqlite'))
+			$this->markTestSkipped('PDO and SQLite extensions are required.');
+
+		$this->_connection=new CDbConnection('sqlite::memory:');
+		$this->_connection->active=true;
+		$this->_connection->pdoInstance->exec(file_get_contents(dirname(__FILE__).'/db/data/sqlite.sql'));
+		CActiveRecord::$db=$this->_connection;
+		$transaction = $this->_connection->beginTransaction();
+		for($i = 6; $i <= 10000; $i++) {
+			$this->_connection->getCommandBuilder()->createInsertCommand(
+				"posts",
+				array(
+					"title" => "post ".$i,
+					"content" => "content ".$i,
+					"create_time" => 100000 + $i,
+					"author_id" => 1
+				)
+			)->execute();
+		}
+		$transaction->commit();
+	}
+
+	protected function tearDown()
+	{
+		$this->_connection->active=false;
+	}
+
+	public function testSpeed() {
+		$startTime = microtime(true);
+		$reader = $this->_connection->getCommandBuilder()->createFindCommand(
+			"posts",
+			Post::model()->getDbCriteria()
+		)->query();
+		$i = 0;
+		while(($row = $reader->read()) !== false) {
+			$post = Post::model()->populateRecord($row);
+			$this->assertEquals("post ".($i + 1),"$post->title");
+			$i++;
+		}
+		$endTime = microtime(true);
+		echo __CLASS__." in ".($endTime - $startTime)." seconds (".memory_get_peak_usage()." bytes)\n";
+	}
+}
